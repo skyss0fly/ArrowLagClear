55677e2ad4554d5da001e23e189a1e9cc06fbcb0
AquaTheCoder <107248565+MadaraUchihaHZ@users.noreply.github.com>
2023-08-11 15:40:15 +0530

    Create ArrowClear.php

---
+++ src/ArrowClear/ArrowClear.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ArrowClear;
+
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\entity\projectile\Arrow;
+
+class ArrowClear extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onProjectileHit(ProjectileHitEvent $event): void {
+        $entity = $event->getEntity();
+        if ($entity instanceof Arrow) {
+            $this->getScheduler()->scheduleDelayedTask(new ClearArrowTask($entity), 40); // 20 ticks per second, 2 seconds delay
+        }
+    }
+}
+
+class ClearArrowTask extends \pocketmine\scheduler\Task {
+
+    private $arrow;
+
+    public function __construct(Arrow $arrow) {
+        $this->arrow = $arrow;
+    }
+
+    public function onRun(): void {
+        if (!$this->arrow->isClosed()) {
+            $this->arrow->flagForDespawn();
+        }
+    }
+}
